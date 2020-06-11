80fab5d869fde1300116ca1052a875ca91b41ca3
cflopezl <cflopezl@gmail.com>
2020-06-10 18:48:56 -0600

    Fin sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,17 +8,30 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
 
   info: InfoPagina = {};
+  equipo: any = [];
   cargada = false;
 
   constructor( private http: HttpClient) {
     console.log("llamada al servicio");
-    //Leer archivo JSON
-    this.http.get('assets/data/data-pagina.json')
+    this.cargarInfo();
+    this.cargarEquipo();
+
+   }
+
+   private cargarInfo(){
+      this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {
         this.cargada=true;
         this.info=resp;
-        console.log(resp);
       });
+   }
+
+   private cargarEquipo(){
+    this.http.get('https://angular-html-e9d7e.firebaseio.com/equipo.json')
+    .subscribe( resp  => {
+      this.equipo=resp;
+      console.log(resp);
+    });
 
    }
 }
